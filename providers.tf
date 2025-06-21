f84e5bd45c0d0e44b828329dc1448d42bc728d06
Konstantin Chigridov <werest54@rambler.ru>
2025-06-21 13:09:21 +0300

    backend s3

--- providers.tf
+++ providers.tf
@@ -5,6 +5,25 @@ terraform {
     }
   }
   required_version = "~>1.8.4"
+
+  backend "s3" {
+    region   = "ru-central1"
+    bucket   = "backet-werest-develop"
+    key      = "terraform.state"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+
+    endpoints = {
+      dynamodb = "https://docapi.serverless.yandexcloud.net/ru-central1/b1g5lq99m43jv5mpei89/etnlegk15et9uh8c8tjm"
+      s3       = "https://storage.yandexcloud.net"
+    }
+
+    dynamodb_table = "tfstate-werest-develop-2106"
+  }
+
 }
 
 provider "yandex" {
